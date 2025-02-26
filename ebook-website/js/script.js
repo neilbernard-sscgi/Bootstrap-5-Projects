f36323e79a27da9fad6d916af852d3f6ead75560
neilbernard-sscgi <nbsupillo@sscgi.com>
2025-02-26 08:47:02 +0800

    added mini projects

--- ebook-website/js/script.js
+++ ebook-website/js/script.js
@@ -1,15 +1,15 @@
 function userScroll() {
-  const navbar = document.querySelector('.navbar');
+  const navbar = document.querySelector(".navbar");
 
-  window.addEventListener('scroll', () => {
+  window.addEventListener("scroll", () => {
     if (window.scrollY > 50) {
-      navbar.classList.add('bg-dark');
-      navbar.classList.add('navbar-sticky');
+      navbar.classList.add("bg-dark");
+      navbar.classList.add("navbar-sticky");
     } else {
-      navbar.classList.remove('bg-dark');
-      navbar.classList.remove('navbar-sticky');
+      navbar.classList.remove("bg-dark");
+      navbar.classList.remove("navbar-sticky");
     }
   });
 }
-
-document.addEventListener('DOMContentLoaded', userScroll);
+document.getElementById("year").textContent = new Date().getFullYear();
+document.addEventListener("DOMContentLoaded", userScroll);
